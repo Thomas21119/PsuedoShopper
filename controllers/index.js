e574cc7b2af0a7c14453afacd68f9b75c9d1085f
Thomas21119 <thomas.wittwer2018@gmail.com>
2021-11-25 20:51:28 +1100

    utils folder with authorise in it and commented out helpers in controllers/api as we dont have it yet

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const homePage = require("./homePage");
+const homePage = require('./homePage');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use("/", homePage);
+// router.use("/", homePage);
 
 module.exports = router;
