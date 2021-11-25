bc37dbf7be811a86c7dab247a6213c7307cedc88
TauH <thaywood988@gmail.com>
2021-11-25 19:49:23 +1100

    updated serever.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
@@ -26,7 +25,6 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
