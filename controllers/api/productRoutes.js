a38869a9913942620ba5ab1819bdb122e500dc72
TauH <thaywood988@gmail.com>
2021-11-30 22:32:03 +1100

    l;'

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -13,14 +13,9 @@ router.put("/buy/:id", withAuth, async (req, res) => {
     console.log("before", productPurchased);
     productPurchased.user_id = req.session.user_id;
     productPurchased.forSale = false;
-<<<<<<< HEAD
 
     console.log("after", productPurchased);
     res.status(200).json(productPurchased);
-=======
-    console.log(productPurchased);
-    res.status(200).json("after", productPurchased);
->>>>>>> 25bec64256a6aed356a3fec18c257c78a40d20a6
   } catch (err) {
     res.status(500).json(err);
   }
