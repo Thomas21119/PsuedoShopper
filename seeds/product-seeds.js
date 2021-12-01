09ee609043d8730d18f8d990ef659786486b8d70
Ruchi <ruchisaini.0404@gmail.com>
2021-12-01 15:07:24 +1100

    added Card Style on Products

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -5,6 +5,7 @@ const productData = [
     category_id: 1,
     user_id: 1,
     cost: 50,
+    forSale: true,
   },
   {
     category_id: 2,
@@ -19,21 +20,20 @@ const productData = [
     forSale: true,
   },
   {
-    category_id: 3,
+    category_id: 4,
     user_id: 4,
     cost: 150,
-    forSale: true,
   },
   {
     category_id: 4,
-    user_id: 1,
+    user_id: 5,
     cost: 666,
+    forSale: true,
   },
   {
     category_id: 4,
-    user_id: 2,
+    user_id: 6,
     cost: 40,
-    forSale: true,
   },
   {
     category_id: 5,
@@ -45,7 +45,6 @@ const productData = [
     category_id: 5,
     user_id: 4,
     cost: 10,
-    forSale: true,
   },
   {
     category_id: 6,
@@ -55,7 +54,7 @@ const productData = [
   },
   {
     category_id: 8,
-    user_id: 2,
+    user_id: 5,
     cost: 25,
     forSale: true,
   },
@@ -66,7 +65,7 @@ const productData = [
     forSale: true,
   },
   {
-    category_id: 3,
+    category_id: 7,
     user_id: 4,
     cost: 10,
     forSale: true,
@@ -75,7 +74,6 @@ const productData = [
     category_id: 2,
     user_id: 1,
     cost: 10,
-    forSale: true,
   },
 ];
 
