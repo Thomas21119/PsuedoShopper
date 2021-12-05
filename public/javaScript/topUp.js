03dd2d5359e6a10840435a8d78dafb8723127717
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-12-05 21:33:41 +1100

    updt: added validation on top-up page

--- public/javaScript/topUp.js
+++ public/javaScript/topUp.js
@@ -1,26 +1,59 @@
+const amountTxt = document.querySelector("#topUpAmount");
+const cardNoTxt = document.querySelector("#cardNumber");
+const topUpFrm = document.querySelector("#topUpSubmit");
+
 const topUp = async (event) => {
   event.preventDefault();
   try {
-    const amount = document.querySelector("#topUpAmount").value;
+    
+    const amount = amountTxt.value;
+    const cardNo = cardNoTxt.value;
+
+console.log(cardNo);
+
     if (!amount) {
-      console.log("error");
-      alert("error");
+      alert("Enter a top-up amount");
+      amountTxt.focus();
       return;
     }
-    const topUp = await fetch("/api/wallet/walletTopUp", {
-      method: "PUT",
-      body: JSON.stringify({
-        amount,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (topUp.ok) {
-      console.log("test");
-      document.location.replace("/wallet");
+    if (!cardNo) {
+      alert("Enter a value card number");
+      cardNoTxt.focus();
+      return;
     }
-  } catch (err) {}
+    
+    if (cardNo) {
+      if (cardNo.length < 16) {
+        alert("Enter a value card number");
+        cardNoTxt.focus();
+        return;
+      }else if (cardNo.length > 16) {
+        alert("Enter a value card number");
+        cardNoTxt.focus();
+        return;
+      }else {
+          const topUp = await fetch("/api/wallet/walletTopUp", {
+            method: "PUT",
+            body: JSON.stringify({
+              amount,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          if (topUp.ok) {
+            alert(`Your top-up was successful. Added $ ${amount} to your account.`)
+            location.reload();
+            window.location.replace("/wallet");
+            amountTxt.focus();
+          }
+        }
+      }
+  } catch (err) {
+    alert(err);
+  }
 };
 
-document.querySelector("#topUpSubmit").addEventListener("submit", topUp);
+amountTxt.focus();
+
+topUpFrm.addEventListener("submit", topUp);
